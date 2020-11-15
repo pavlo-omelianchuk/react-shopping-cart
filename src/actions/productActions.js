dc419ca5f6ddfc3a054d2da5f8f2f4d02eccfa9f
YoussefPasha <jojoyossefbasha010@gmail.com>
2020-11-15 21:25:21 +0200

    create filteration into redux reducer with action

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,4 +1,8 @@
-import { FETCH_PRODUCTS } from "../types";
+import {
+  FETCH_PRODUCTS,
+  FILTER_PRODUCTS_BY_SIZE,
+  ORDER_PRODUCTS_BY_Price,
+} from "../types";
 
 export const fetchProducts = () => async (dispatch) => {
   const res = await fetch("/api/products");
@@ -6,5 +10,42 @@ export const fetchProducts = () => async (dispatch) => {
   dispatch({
     type: FETCH_PRODUCTS,
     payload: data,
+  });
+};
+
+export const filterProducts = (products, size) => async (dispatch) => {
+  dispatch({
+    type: FILTER_PRODUCTS_BY_SIZE,
+    payload: {
+      size: size,
+      items:
+        size === ""
+          ? products
+          : products.filter((x) => x.availableSizes.indexOf(size)),
+    },
+  });
+};
+
+export const sortProducts = (filterProducts, sort) => async (dispatch) => {
+  const sortedProducts = filterProducts.slice();
+  if (sort === "") {
+    sortProducts.sort((a, b) => (a._id > b._id ? 1 : -1));
+  } else {
+    sortProducts.sort((a, b) =>
+      sort === "lowestprice"
+        ? a.price > b.price
+          ? 1
+          : -1
+        : a.price > b.price
+        ? -1
+        : 1
+    );
+  }
+  dispatch({
+    type: ORDER_PRODUCTS_BY_Price,
+    payload: {
+      sort: sort,
+      items: sortedProducts,
+    },
   });
 };
--- src/reducers/productReducers.js
+++ src/reducers/productReducers.js
@@ -1,10 +1,25 @@
-import { FETCH_PRODUCTS } from "../types";
+import {
+  FETCH_PRODUCTS,
+  FILTER_PRODUCTS_BY_SIZE,
+  ORDER_PRODUCTS_BY_Price,
+} from "../types";
 
 export const productsReducer = (state = {}, action) => {
   switch (action.type) {
     case FETCH_PRODUCTS:
       return { items: action.payload };
-
+    case FILTER_PRODUCTS_BY_SIZE:
+      return {
+        ...state,
+        size: action.payload.size,
+        filteredItems: action.payload.items,
+      };
+    case ORDER_PRODUCTS_BY_Price:
+      return {
+        ...state,
+        sort: action.payload.sort,
+        filteredItems: action.payload.items,
+      };
     default:
       return state;
   }
