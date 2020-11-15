d432bcd099f0ac5496f4f26695fe8dbae6325dc2
YoussefPasha <jojoyossefbasha010@gmail.com>
2020-11-15 23:57:07 +0200

    done add order to server

--- server.js
+++ server.js
@@ -40,5 +40,46 @@ app.delete("/api/products/:id", async (req, res) => {
   res.send(deletedProduct);
 });
 
+const Order = mongoose.model(
+  "order",
+  new mongoose.Schema(
+    {
+      _id: {
+        type: String,
+        default: shortId.generate,
+      },
+      email: String,
+      name: String,
+      address: String,
+      total: Number,
+      cartItems: [
+        {
+          _id: String,
+          title: String,
+          price: Number,
+          count: Number,
+        },
+      ],
+    },
+    {
+      timestamps: true,
+    }
+  )
+);
+
+app.post("/api/orders", async (req, res) => {
+  if (
+    !req.body.name ||
+    !req.body.email ||
+    !req.body.address ||
+    !req.body.total ||
+    !req.body.cartItems
+  ) {
+    return res.send({ message: "Data is Required." });
+  }
+  const order = await Order(req.body).save();
+  res.send(order);
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log("http://localhost:5000"));
